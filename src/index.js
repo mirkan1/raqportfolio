d16e8dc146c6d05c4bff851b9250018589c9db71
mirkan <mirkanbaba1@gmail.com>
2019-09-10 22:59:57 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,62 @@
-import React from 'react';
+﻿import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Menu extends React.Component {
+  componentDidMount() {
+    let x = document.getElementsByTagName('li')[0].innerHTML
+    console.log(x)
+    // bunu al iste sayfalari buna gore cek
+    // basit bir brosur sitesi olacak 
+    // sonra ise alinacan mirkan kasma yap bi seyler
+  }
+  render() {
+    return (
+      <div className="menu-list">
+        <ul>
+          <li id="aboutme" onClick={this.props.clickedList}  style={{"margin": "0 0 0 0",}}>About me</li>
+          <li id="myprojects" onClick={this.props.clickedList} >My projects</li>
+          <li id="tag3" onClick={this.props.clickedList} >My past</li>
+          <li id="tag4" onClick={this.props.clickedList} >Tag4</li>
+          <li id="tag5" onClick={this.props.clickedList} >Tag5</li>
+        </ul>
+      </div>
+    )
+  }
+}
+class Main extends React.Component {
+  clickedList(e) {
+    let margin = 100
+    let id = document.getElementById(e.target.id)
+    let paddingInterval = setInterval(
+      () => {
+        console.log(margin)
+        id.setAttribute("style", `padding-top: ${margin}px`)
+        //id.setAttribute("style", `margin-right: ${margin}px`)
+        margin = margin + 100
+        if (margin >= 500) {
+          clearInterval(paddingInterval)
+        }
+      }, 100)
+    //console.log(ReactDOM.findDOMNode(this).innerHTML)
+  }
+  render() {
+    return (
+      <html>
+        <body>
+          <Menu clickedList={this.clickedList}/>
+        </body>
+      </html>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // ========================================
+  
+  ReactDOM.render(
+    <Main />,
+    document.getElementById('root')
+  );
+  
+// TO CREATE: npx create-react-app my-app
+// TO RUN: npm start 
